f4b71dcb97897c99687ae80caa441b59d7a09a5f
Mike-at-TRU <michael_lebrun@outlook.com>
2024-03-01 21:19:41 -0800

    why is this not working?

--- scripts/MemoryCard.js
+++ scripts/MemoryCard.js
@@ -2,36 +2,53 @@ export class MemoryCard extends HTMLElement {
     #isFliped = false;
     #isRemoved = false;
 
-    constructor(id, image, cardValue) {
+    constructor(id, frontImage, background, cardValue) {
         super();
         this.id = id;
-        this.image = image;
+        this.frontImage = frontImage;
+        this.background = background;
         this.cardValue = cardValue; //this was privite and I couldn't understand why it kept saying undefined 
     }
 
-    // hide() {
-    //     this.#isFliped = false;
-    // }
+    hide() {
+        this.#isFliped = false;
+    }
 
-    // reveal() {
-    //     this.#isFliped = true;
-    // }
+    reveal() {
+        this.#isFliped = true;
+    }
 
-    // isFilped() {
-    //     return this.#isFliped;
-    // }
+    isFilped() {
+        return this.#isFliped;
+    }
 
-    // isEqual(memoryCardToCheck) {
-    //     return this.cardValue === memoryCardToCheck.cardValue;
-    // }
+    isEqual(memoryCardToCheck) {
+        return this.cardValue === memoryCardToCheck.cardValue;
+    }
 
-    // remove() {
-    //     this.#isRemoved = true;
-    // }
+    remove() {
+        this.#isRemoved = true;
+    }
 
-    // isRemoved() {
-    //     return this.#isRemoved;
-    // }
+    isRemoved() {
+        return this.#isRemoved;
+    }
+    connectedCallback() {
+        //get the template
+        const template = document.querySelector("#cardTemplate");
+
+        //clone it's content
+        const contentNode = template.content.cloneNode(true);
+
+        //bind some data
+
+
+        //TODO Populate the 
+
+
+        //append to DOM
+        this.appendChild(contentNode);
+    }
 
 
 }
--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,16 @@ import { MemoryCard } from "./MemoryCard.js";
 
 customElements.define("memory-cards", MemoryCard);
 
-const testMeomoryCards = [{
-    image: `./assets/Black_Cards/AceOfHearts.png`
-}, { image: `./assets/Black_Cards/Background.png` }];
+const testMeomoryCards = [];
+for (let index = 0; index < 52; index++) {
+    testMeomoryCards.push({
+        frontImage: `./assets/blackcards${index}.png`,
+        background: `.assets/blackcards/background.png`
+    });
+}
+
+
+console.log({ testMeomoryCards });
 function shuffle(array) {
     const length = array.length;
     let randomPositionToSwap;
@@ -33,19 +40,23 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
     const numberOfPairsWithMatches = 1;
 
-    const allCardData = testMeomoryCards.flatMap(
-        ({ image }, index) => Array(numberOfPairsWithMatches * 2).fill(0).map(
-            (_) => ({ image, value: index })
+    const usedCards = shuffle(testMeomoryCards).filter((_, index) => index < 10);
+
+    console.log({ usedCards });
+
+    const allCardData = usedCards.flatMap(
+        ({ frontImage, background }, index) => Array(numberOfPairsWithMatches * 2).fill(0).map(
+            (_) => ({ frontImage, background, value: index })
         )
         //[{ image, value: index }, { image, value: index }]
     );
-    console.log(allCardData);
+    console.log({ allCardData });
     const shuffledCardData = shuffle(allCardData);
-    console.log(shuffledCardData);
+    console.log({ shuffledCardData });
 
     const cardMap = shuffledCardData.reduce((acc, nextCard, id) => {
         if (!acc.has(id)) {
-            acc.set(id, new MemoryCard(id, nextCard.image, nextCard.value));
+            acc.set(id, new MemoryCard(id, nextCard.frontImageimage, nextCard.background, nextCard.value));
             return acc;
         }
         throw Error("this ID already has been used");
@@ -54,49 +65,44 @@ document.addEventListener(`DOMContentLoaded`, () => {
     let lastPickedCardID;
     let bothCardsFlipped;
     let gameWon = false;
-    allTheCardElements.forEach(cardElement => cardElement.innerHTML = `<img src='${cardImgPicked + 'Background.png'}' class="memoryCardImage">`);
 
-    const allTheCards = Array.from(allTheCardElements).reduce((cardElements, next) => {
-        if (!cardElements.has(next.id)) {
-            cardElements.set(next.id, new MemoryCard(1));
-            return cardElements;
-        }
-        throw Error("this ID already has been used");
-    }, new Map());
+    // let allTheCardElements = Array.from(cardMap).map(() => document.createElement("memory-cards"));
+    // allTheCardElements = allTheCardElements.map((card, id) => card.id = id);
+    const allTheCardElements = document.createElement(`memory-cards`);
+    console.log({ allTheCardElements });
 
 
-
-    allTheCardElements.forEach(cardElement => cardElement.addEventListener('click', () => {
-        const id = cardElement.id;
-        const card = allTheCards.get(id);
-
-
-        if (id === lastPickedCardID) {
-            console.log('Sorry, That last card was already chosen, please choose another card');
-        }
-
-        else if (!(card.isFilped())) {
-
-            if (bothCardsFlipped) {
-                flipLastCardsBack();
-
-            }
-            card.reveal();
-            cardElement.innerHTML = `<img src='${cardImgPicked + 'AceOfHearts.png'}' class="memoryCardImage">`;
-            //card.cardValue;
-            bothCardsFlipped = returnNotBool(bothCardsFlipped);
-
-            if (bothCardsFlipped) {
-                checkForMatches(id, lastPickedCardID);
-            }
-            lastPickedCardID = id;
+    // allTheCardElements.forEach(cardElement => cardElement.addEventListener('click', () => {
+    //     const id = cardElement.id;
+    //     const card = allTheCards.get(id);
 
 
-        }
+    //     if (id === lastPickedCardID) {
+    //         console.log('Sorry, That last card was already chosen, please choose another card');
+    //     }
+
+    //     else if (!(card.isFilped())) {
+
+    //         if (bothCardsFlipped) {
+    //             flipLastCardsBack();
+
+    //         }
+    //         card.reveal();
+    //         cardElement.innerHTML = `<img src='${cardImgPicked + 'AceOfHearts.png'}' class="memoryCardImage">`;
+    //         //card.cardValue;
+    //         bothCardsFlipped = returnNotBool(bothCardsFlipped);
+
+    //         if (bothCardsFlipped) {
+    //             checkForMatches(id, lastPickedCardID);
+    //         }
+    //         lastPickedCardID = id;
+
+
+    //     }
 
 
 
-    }));
+    // }));
 
 
     function returnNotBool(boolToCheck) {
